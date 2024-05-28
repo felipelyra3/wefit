6e3a4518b7a48b6471c5db9e7ed98a49eb0ba5c9
Felipe Lyra <felipelyra3@gmail.com>
2024-05-28 16:00:07 -0300

    Beginning header

--- wefit/src/App.js
+++ wefit/src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import "./Styles/ResetCss.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './Components/Homepage/homepage.js';
+import Header from './Components/Header/header.js';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header></Header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ wefit/src/Components/Header/header.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+export default function Header() {
+    return (
+        <>
+            <Page>
+                <h1>WeFit</h1>
+            </Page>
+        </>
+    );
+}
+
+const Page = styled.div`
+    background-color: lightblue;
+`;
