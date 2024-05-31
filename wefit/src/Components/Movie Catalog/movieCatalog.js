0d66a6a95aa4ceeacf40da02cf1c73f00798473a
Felipe Lyra <felipelyra3@gmail.com>
2024-05-31 15:39:24 -0300

    Movie covers

--- wefit/src/Components/Movie Catalog/movieCatalog.js
+++ wefit/src/Components/Movie Catalog/movieCatalog.js
@@ -1,5 +1,87 @@
-export default function MovieCatalog() {
+import styled from "styled-components";
+import React, { useContext } from 'react';
+import UserContext from "../Contexts/UserContext.js";
+import { movieList } from "./movieList.js";
+import { SlBasket } from "react-icons/sl";
+
+function MovieRender({ movie }) {
+    const { itens, setItens } = useContext(UserContext);
     return (
-        <>MOVIE CATALOG</>
+        <>
+            <Movie>
+                <ul>
+                    <li><img src={movie.image} /></li>
+                    <li>{movie.name}</li>
+                    <li>{movie.price}</li>
+                </ul>
+                <ButtonAdicionarAoCarrinho><CartItemContainer><SlBasket /><span>{itens}</span></CartItemContainer> Adicionar ao carrinho</ButtonAdicionarAoCarrinho>
+            </Movie>
+        </>
     );
 }
+
+export default function MovieCatalog() {
+    return (
+        <>
+            <Page>
+                <MoviesContainer>{movieList != 0 ? <>{movieList.map((movie, key) => (<MovieRender movie={movie} />))}</> : <> NENHUM FILME CADASTRADO</>}</MoviesContainer>
+            </Page>
+        </>
+    );
+}
+
+const Page = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const MoviesContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 80%;
+    height: 100%;
+`
+
+
+const Movie = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #FFFFFF;
+    color: #000000;
+    height: 300px;
+    width: 300px;
+    border-radius: 4px;
+    margin: 12px;
+
+    li {
+        display: flex;
+        justify-content: center;
+        margin: 4px 0px 4px 0px;
+        color: #000000;
+        font-weight: bold;
+    }
+`;
+
+const ButtonAdicionarAoCarrinho = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90%;
+    height: 40px;
+    background-color: #009EDD;
+    color: #FFFFFF;
+    border: none;
+    border-radius: 5px;
+`;
+
+const CartItemContainer = styled.div`
+    margin: 0px 8px 0px 0px;
+
+    span {
+        margin: 0px 0px 0px 5px;
+    }
+`;
---
+++ wefit/src/Components/Movie Catalog/movieList.js
@@ -0,0 +1,45 @@
+import viuvaNegra from "../../Assets/Movie Covers/01viuvaNegra.png";
+import homemAranha from "../../Assets/Movie Covers/02homemAranha.png";
+import shangChi from "../../Assets/Movie Covers/03shangChi.png";
+import morbius from "../../Assets/Movie Covers/04morbius.png";
+import batman from "../../Assets/Movie Covers/05batman.png";
+import eternos from "../../Assets/Movie Covers/06eternos.png";
+
+export const movieList = [
+    {
+        id: 1,
+        image: viuvaNegra,
+        name: "Viúva Negra",
+        price: 9.99,
+    },
+    {
+        id: 2,
+        image: homemAranha,
+        name: "Homem Aranha",
+        price: 29.99,
+    },
+    {
+        id: 3,
+        image: shangChi,
+        name: "Shang-Chi",
+        price: 30.99,
+    },
+    {
+        id: 4,
+        image: morbius,
+        name: "Morbius",
+        price: 9.99,
+    },
+    {
+        id: 5,
+        image: batman,
+        name: "O Batman",
+        price: 30.99,
+    },
+    {
+        id: 6,
+        image: eternos,
+        name: "Eternos",
+        price: 29.99,
+    },
+];
